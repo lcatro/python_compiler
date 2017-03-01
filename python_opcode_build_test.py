80867f9a8b16ea1634208e4c6945711f67634207
lcatro <lcatro@sina.cn>
2017-03-01 15:56:16 +0800

    add python instruction test code

---
+++ python_opcode_build_test.py
@@ -0,0 +1,18 @@
+
+const_list_start
+AAAAAAAA
+const_list_end
+
+name_list_start
+
+name_list_end
+
+variant_name_list_start
+
+variant_name_list_end
+
+LOAD_CONST 0
+PRINT_ITEM
+PRINT_NEWLINE
+LOAD_CONST None
+RETURN_VALUE
