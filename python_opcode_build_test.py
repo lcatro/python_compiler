7c14ef6a4c6ff1ce84fd247e886bf79261c540d6
lcatro <lcatro@sina.cn>
2017-03-14 21:17:40 +0800

    new test case

--- python_opcode_build_test.py
+++ python_opcode_build_test.py
@@ -1,22 +1,20 @@
 
-config_start
-stacksize=10
-config_end
-
-const_list_start
-AAAAAAAA
-const_list_end
-
-name_list_start
-
-name_list_end
-
-variant_name_list_start
-
-variant_name_list_end
-
-LOAD_CONST 0
+LOAD_CONST 'try to load function ..'
 PRINT_ITEM
 PRINT_NEWLINE
-LOAD_CONST None
-RETURN_VALUE
+
+function try_print ()
+LOAD_CONST 'AAA'
+PRINT_ITEM
+PRINT_NEWLINE
+return
+
+LOAD_CONST 'try to call function ..'
+PRINT_ITEM
+PRINT_NEWLINE
+
+CALL_FUNCTION 'try_print'
+
+LOAD_CONST 'call function end ..'
+PRINT_ITEM
+PRINT_NEWLINE
